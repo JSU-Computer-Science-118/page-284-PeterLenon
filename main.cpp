37aa20842b8a6ce6aabc51cdb1efce8a79173705
PeterLenon <112224594+PeterLenon@users.noreply.github.com>
2022-11-18 01:53:26 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,21 @@
-// write your code here
+#include <ctime>
+#include <cstdlib>
+#include <iostream>
+
+using namspace std;
+
+int main()
+{
+  int num;
+  cin>>num;
+  seed = time(0);
+  srand(seed);
+  for (int i =0; i< num; i++)
+  {
+    cout<<rand()<<" "<<endl;
+  }
+  return 0;
+}
+  
+    
+  
